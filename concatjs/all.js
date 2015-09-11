1d3ebce5e31089005500c1e75e1a8d0d05edfa84
Danny <gqwebsites@gmail.com>
2015-09-11 16:28:24 -0400

    Fixed for mobile first draft

--- concatjs/all.js
+++ concatjs/all.js
@@ -23,3 +23,28 @@
 
 // Place any jQuery/helper plugins in here.
 
+// to top right away
+if ( window.location.hash ) scroll(0,0);
+// void some browsers issue
+setTimeout( function() { scroll(0,0); }, 1);
+
+$(function() {
+	var navHeight = $('#nav').height();
+    // your current click function
+    $('.scroll').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top -(navHeight)
+        }, 1000, 'swing');
+    });
+
+    // *only* if we have anchor on the url
+    if(window.location.hash) {
+
+        // smooth scroll to the anchor id
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top -(navHeight)
+        }, 1000, 'swing');
+    }
+
+});
